b4b598972d7f64916f655500588d0121e5f72256
Ruchit Patel <ruchitnpatel@gmail.com>
2021-04-12 22:27:29 +0000

    hello kernel done

--- src/00-hello-kernel.c
+++ src/00-hello-kernel.c
@@ -6,11 +6,17 @@ static int __init entry_point(void) {
 	return 0;
 }
 
-static void __exit my_exit(void) {
+static void __exit exit_point(void) {
 	printk(KERN_INFO "Bye, kernel!\n");
 }
-
 
 module_init(entry_point);
 module_exit(exit_point);
 
+MODULE_DESCRIPTION("Hello, Kernel Module.");			// explore this functions
+MODULE_AUTHOR("Ruchit Patel");
+MODULE_LICENSE("GPL");
+
+// continue: messing around with the program (check priv level)
+// understand make more in detail (all files created by it)
+// use modinfo with this
