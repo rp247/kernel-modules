87f9f7f5c90521e52c07e2137c8242cc10c8485d
Ruchit Patel <ruchitnpatel@gmail.com>
2021-04-12 21:32:44 +0000

    making hello world

--- src/00-hello-kernel.c
+++ src/00-hello-kernel.c
@@ -1,1 +1,16 @@
-// continue
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+static int __init entry_point(void) {
+	printk(KERN_INFO "Hello, kernel!\n");
+	return 0;
+}
+
+static void __exit my_exit(void) {
+	printk(KERN_INFO "Bye, kernel!\n");
+}
+
+
+module_init(entry_point);
+module_exit(exit_point);
+
