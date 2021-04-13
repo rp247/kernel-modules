6d8fb6b5811fbd15591b891d44eeb79b31ea846c
Ruchit Patel <ruchitnpatel@gmail.com>
2021-04-13 17:43:14 +0000

    log levels

--- src/01-printk.c
+++ src/01-printk.c
@@ -8,8 +8,22 @@
  * https://www.kernel.org/doc/html/latest/core-api/printk-basics.html
  */
 
+
+/* 
+ * log levels and macros
+ * KERN_EMERG		:	pr_emerg()	: 	"0"
+ * KERN_ALERT		:	pr_alert()	:	"1"
+ * KERN_CRIT		:	pr_crit()	:	"2"
+ * KERN_ERR		:	pr_err()	:	"3"
+ * KERN_WARNING		:	pr_warn()	:	"4"
+ * KERN_NOTICE		:	pr_notice()	:	"5"
+ * KERN_INFO		:	pr_info()	:	"6"
+ * KERN_DEBUG		:	pr_debug()	:	"7"
+ */
+
+
 static int __init pk_entry(void) {
-	printk(KERN_INFO "PRITNK!");
+	printk(KERN_EMERG "Emergency!! (%s)\n", KERN_EMERG); 
 
 	// format: printk(LOG_LEVEL "MESSAGE %SPECIFIER", value);
 
@@ -17,7 +31,7 @@ static int __init pk_entry(void) {
 }
 
 static void __exit pk_exit(void) {
-	printk(KERN_INFO "!KNTIRP");
+	printk(KERN_INFO "!KNTIRP\n");
 }
 
 
