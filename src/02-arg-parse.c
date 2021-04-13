07d2e02f6c8243e956b8b6663a89c91714ad6a97
Ruchit Patel <ruchitnpatel@gmail.com>
2021-04-13 18:30:55 +0000

    arg parse setup

--- src/02-arg-parse.c
+++ src/02-arg-parse.c
@@ -1,2 +1,19 @@
-__init, __initdata, __exit
-MODULE_()
+/* macros __init, __initdata, __exit
+MODULE_() */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+
+static int __init arg_init(void) {
+	return 0;
+}
+
+static void __exit arg_exit(void) {
+}
+
+module_init(arg_init);
+module_exit(arg_exit);
+
+MODULE_DESCRIPTION("Arg passig to modules.");
+MODULE_AUTHOR("Ruchit Patel");
+MODULE_LICENSE("GPL");
