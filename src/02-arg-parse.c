ccff3e745bb2c1d9131279380e74af4495c607c2
Ruchit Patel <ruchitnpatel@gmail.com>
2021-04-13 19:50:29 +0000

    changed description and renamed params

--- src/02-arg-parse.c
+++ src/02-arg-parse.c
@@ -3,10 +3,16 @@ MODULE_() */
 
 #include <linux/kernel.h>
 #include <linux/module.h>
+// #include <linux/stat.h> for macros such as S_IRWXU S_IRUSR S_IWUSR S_IXUSR
+#include <linux/moduleparam.h>			// for module param types
 
 static int  param1; 
+module_param(param1, int, 0644);			// module_parama (name of var, type, permissions for /sys/mod/mod)
+MODULE_PARM_DESC(param1, "Int Param1");			// see /sys/module/02_arg_parse
+module_param_named(paramOne, param1, int, 0);
+MODULE_PARM_DESC(paramOne, "Int ParamOne");		// see /sys/module/02_arg_parse
 
-module_param(param1, int, 0000);		// module_parama (name of var, type, permissions??)
+// static char *str = "str1";
 
 static int __init arg_init(void) {
 	pr_info("Param1: %d\n", param1);
